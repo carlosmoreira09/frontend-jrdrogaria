442e9960674b0ccedf2a6e80358701b0f2bf9398
Carlos Moreira <cesmoreira90@hotmail.com>
2025-04-09 18:57:42 +0100

    changes in configs file

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,24 @@ import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    base: './', // Make sure this is correct for your deployment
+    plugins: [
+        react(),
+        tailwindcss()
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name]-[hash].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash].[ext]',
+            }
+        }
     },
-  },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
