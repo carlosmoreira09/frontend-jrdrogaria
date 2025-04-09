44046af48d057a17c4b31b6bc8c6e0d1d7a02459
Carlos Moreira <cesmoreira90@hotmail.com>
2025-04-09 20:38:10 +0100

    change endpoint point

--- src/lib/interceptor.ts
+++ src/lib/interceptor.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 export const apiClient = axios.create({
-    baseURL: 'http://localhost:3000/api/v1',
+    baseURL: 'http://127.0.0.1/api/v1',
 });
 
 apiClient.interceptors.request.use(
