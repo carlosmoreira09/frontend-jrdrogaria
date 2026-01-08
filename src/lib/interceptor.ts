8f54af03b4093e4c246532e02a6793344f5c36e8
Carlos Moreira <cesmoreira90@hotmail.com>
2026-01-08 08:12:47 +0100

    update endpoint prod to dns
    # Conflicts:
    #	src/lib/interceptor.ts

--- src/lib/interceptor.ts
+++ src/lib/interceptor.ts
@@ -5,36 +5,22 @@ export const apiClient = axios.create({
     baseURL: 'https://api.jrdrogaria.com.br/api/v1',
 });
 
-adminClient.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response?.status === 401) {
-            localStorage.removeItem('admin_token');
-            window.location.href = '/admin/login';
+apiClient.interceptors.request.use(
+    (config) => {
+        const token = Cookies.get('token');
+        const tenant = Cookies.get('tenant');
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
         }
+        console.log(tenant)
+        if (tenant) {
+            config.headers['x-tenant-id'] = tenant;
+        }
+        return config;
+    },
+    (error) => {
         return Promise.reject(error);
     }
 );
-
-export const apiClient = axios.create({
-    baseURL: 'https://jrdrogaria.com.br/api/v1',
-});
-
-apiClient.interceptors.request.use(
-            (config) => {
-                const token = Cookies.get('token');
-                const tenant = Cookies.get('tenant');
-                if (token) {
-                    config.headers['Authorization'] = `Bearer ${token}`;
-                }
-                console.log(tenant)
-                if (tenant) {
-                    config.headers['x-tenant-id'] = tenant;
-                }
-                return config;
-            },
-            (error) => {
-                return Promise.reject(error);
-            });
 
 export default apiClient;
